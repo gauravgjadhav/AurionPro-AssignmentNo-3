f07545a62edea2117473e3d9f83b35f662d26f57
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-06-20 12:30:12 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        while (true)
+        int sum = 0;
+        for (int i = 0; i < args.Length; i++)
         {
-            int randno = Newnum(1, 100);
-            int count = 1;
-            while (true)
-            {
-                Console.Write("Enter a number between 1 and 100(0 to quit):");
-                int input = Convert.ToInt32(Console.ReadLine());
-                if (input == 0) {
-                    Console.WriteLine("Game Exit");
-                    return;
+            int n = Convert.ToInt32(args[i]);
+            sum = sum + n;
 
-                }
-                else if (input < randno)
-                {
-                    Console.WriteLine("Sorry,Too Low ! plz choose higher number.");
-                    ++count;
-                    continue;
-                }
-                else if (input > randno)
-                {
-                    Console.WriteLine("Sorry,Too High ! plz choose lower number.");
-                    ++count;
-                    continue;
-                }
-                else
-                {
-                    Console.WriteLine("Congratulation You guessed it! The number was {0}!",
-                                       randno);
-                    Console.WriteLine("It took you {0} {1}.\n", count,
-                                       count == 1 ? "try" : "tries");
-                    break;
-                }
-            }
         }
-
-    }
-    static int Newnum(int min, int max)
-    {
-        Random random = new Random();
-        return random.Next(min, max);
+        Console.WriteLine("The sum of the number  " + sum);
+        int avarage = sum / 4;
+        Console.WriteLine("The Avarage Of Numbers  " + avarage);
+        int maxvalue = 0;
+        int minvalue = maxvalue;
+        for (int i = 0; i < args.Length; i++)
+        {
+            int n1 = Convert.ToInt32(args[i]);
+            if(maxvalue < n1)
+            {
+                maxvalue = n1;
+                
+            }
+            if (minvalue > n1)
+            {
+                minvalue = n1;
+                
+            }
+            
+            
+        }
+        Console.WriteLine("The min Value in Array is:"+minvalue);
+        
+        Console.WriteLine("The Max Value in Arrau is:"+maxvalue);
     }
 }
